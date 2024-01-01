7003931848307ecb540a2367a3fdc0b3f2c75989
Brooks <brooksyang@outlook.com>
2024-01-01 23:30:58 +0800

    feat: admin page

--- src/api/admin/admin.ts
+++ src/api/admin/admin.ts
@@ -4,7 +4,11 @@ import { ListAdminResult } from '../model/admin/admin';
 
 enum Api {
   AdminList = '/admins',
+  AdminDetail = '/admins/:name',
 }
 
 export const listAdmin = (params: AccountParams) =>
   defHttp.get<ListAdminResult>({ url: Api.AdminList, params });
+
+export const updateAdminStatus = (username: string, status: number) =>
+  defHttp.put({ url: Api.AdminDetail.replace(':name', username), params: { status } });
