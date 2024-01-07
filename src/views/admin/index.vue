0593f66055fb0dc50a1e080c49c1ec2cf10f215a
Brooks <brooksyang@outlook.com>
2024-01-07 14:54:48 +0800

    feat: update admin roles

--- src/views/admin/index.vue
+++ src/views/admin/index.vue
@@ -64,7 +64,7 @@
   const go = useGo();
   const [registerModal, { openModal }] = useModal();
   const searchInfo = reactive<Recordable>({});
-  const [registerTable, { reload, updateTableDataRecord }] = useTable({
+  const [registerTable, { reload }] = useTable({
     title: '账号列表',
     api: listAdmin,
     rowKey: 'username',
@@ -108,15 +108,18 @@
   }
 
   function handleSuccess({ isUpdate, values }) {
-    if (isUpdate) {
-      // 演示不刷新表格直接更新内部数据。
-      // 注意：updateTableDataRecord要求表格的rowKey属性为string并且存在于每一行的record的keys中
-      console.log(values);
-      const result = updateTableDataRecord(values.username, values);
-      console.log(result);
-    } else {
-      reload();
-    }
+    console.log(values, isUpdate);
+
+    reload();
+    // if (isUpdate) {
+    //   // 演示不刷新表格直接更新内部数据。
+    //   // 注意：updateTableDataRecord要求表格的rowKey属性为string并且存在于每一行的record的keys中
+    //   console.log(values);
+    //   const result = updateTableDataRecord(values.username, values);
+    //   console.log(result);
+    // } else {
+    //   reload();
+    // }
   }
 
   // function handleSelect(deptId = '') {
